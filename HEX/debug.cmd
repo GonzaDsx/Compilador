3387de13da352b913ed37416da129a723dceb259
Gonzalo <jogozarateri@ittepic.edu.mx>
2021-11-26 23:50:31 -0700

    Version 1.8 Version final 1. En proceso de mejoras de la interfaz.

--- HEX/debug.cmd
+++ HEX/debug.cmd
@@ -1,3 +1,3 @@
 c:
 cd C:\Program Files (x86)\Arduino
-arduino_debug.exe --verify C:\Users\DELL\Desktop\Objeto\Sketch\Objeto\Objeto.ino
+arduino_debug.exe --upload C:\Users\DELL\Desktop\Objeto\Sketch\Objeto\Objeto.ino
